8d41326812fb0590b158bde06b19552d9536151a
Szabson123 <szymonzaba18@gmail.com>
2024-10-21 09:59:18 +0200

    added countries to trip

--- trip/models.py
+++ trip/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 from user.models import CustomUser
 from money.models import TripBudget
+import pycountry
+
+
+COUNTRIES = [('None', 'None')] + [(country.alpha_2, country.name) for country in pycountry.countries]
+
 
 class Item(models.Model):
     name = models.CharField(max_length=255)
@@ -22,6 +27,7 @@ class NeededList(models.Model):
 class Trip(models.Model):
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, default=None)
     name = models.CharField(max_length=255)
+    country = models.CharField(choices=COUNTRIES, default=None, max_length=255, blank=True)
     item_list = models.ForeignKey(NeededList, on_delete=models.CASCADE, blank=True, null=True)
     budget = models.OneToOneField(TripBudget, on_delete=models.CASCADE, blank=True, null=True)
     
