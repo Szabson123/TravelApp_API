0647225669935cebbceb7abc49a87d3c28155076
Szabson123 <szymonzaba18@gmail.com>
2024-10-22 08:45:54 +0200

    fixed places

--- place/urls.py
+++ place/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path, include
-from .views import RouteViewSet 
+from .views import RouteViewSet, PlacesViewSet 
 
 from rest_framework.routers import DefaultRouter
 
 router=DefaultRouter()
 
 router.register(r'(?P<trip_id>\d+)/routes', RouteViewSet, basename='trip-routes')
-router.register(r'(?P<route_id>\d+)/places', RouteViewSet, basename='places')
+router.register(r'(?P<route_id>\d+)/places', PlacesViewSet, basename='places')
 
 urlpatterns = [
     path('', include(router.urls))
